d30c9ad20da419ce232eb1544187e36602b2537a
Ciarán Malone <ciaranmalone27@gmail.com>
2023-12-06 14:09:08 +0000

    player can no longer escape my gaze

--- scenes/EnemySpawnPattern/SpawnerGrid.gd
+++ scenes/EnemySpawnPattern/SpawnerGrid.gd
@@ -13,11 +13,10 @@ func spawn_grid():
 	
 	for x in range(1,grid_size+1):
 		for y in range(1,grid_size+1):
-
+			for z in range(1,grid_size+1):	
 				var obj = enemy.instantiate()
 				get_tree().root.add_child(obj)
-				obj.global_position = global_position + Vector3(spacing*x,0,spacing*y)
-				obj.global_position.y = 21
-
+				obj.global_position = global_position + Vector3(spacing*x,spacing*y,spacing*z)
+	
 func _on_timer_timeout():
 	spawn_grid()
--- scripts/PlayerController.gd
+++ scripts/PlayerController.gd
@@ -7,6 +7,9 @@ const HEAVEN_SPEED = 20
 @onready var animation_tree = $ship_idle/AnimationPlayer/AnimationTree
 @export var blend_animations: bool = false
 
+var _3d_vec_pos = Vector3(0,10,32)
+
+
 func _ready():
 	_mode_Swap(GameManager.is_heaven)
 	GameManager.Swap_Mode.connect(_mode_Swap)
@@ -24,20 +27,19 @@ func _process(delta):
 	position.z = max(position.z, GameManager.WORLD_BORDER_Z_MIN)
 	position.z = min(position.z, GameManager.WORLD_BORDER_Z_MAX)
 	
-	if GameManager.is_heaven:
-		if position.x == GameManager.WORLD_BORDER_X_MIN:
-			change_direction(true)
-		
-		elif position.x == GameManager.WORLD_BORDER_X_MAX:
-			change_direction(false)
+	position.y = max(position.y, GameManager.WORLD_BORDER_Y_MIN)
+	position.y = min(position.y, GameManager.WORLD_BORDER_Y_MAX)
 
 func _mode_Swap(is_heaven):
+	
+	velocity = Vector3.ZERO
+	position = Vector3(position.x,_3d_vec_pos.y, _3d_vec_pos.z)
 	if is_heaven:
 		$AnimationPlayer.play("shrink")
+		
 	else: 
 		$AnimationPlayer.play_backwards("shrink")
 		
-var direction_hell = Vector3(1, 0, 0)
 
 func heaven_control(delta): 
 	var input_dir = Input.get_vector("LEFT", "RIGHT", "UP", "DOWN")
@@ -57,6 +59,7 @@ func heaven_control(delta):
 		animation_tree.set("parameters/blend_position", input_dir)
 	
 	move_and_slide()
+	
 func hell_control(delta):
 	var input_dir = Input.get_vector("LEFT", "RIGHT", "UP", "DOWN")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
@@ -75,12 +78,3 @@ func hell_control(delta):
 		animation_tree.set("parameters/blend_position", input_dir)
 	
 	move_and_slide()
-	
-func _input(event):
-	
-	if event.is_action_pressed("SHOOT") && GameManager.is_heaven:
-		print("is_heaven")
-		direction_hell = direction_hell*-1
-
-func change_direction(left):
-	direction_hell = Vector3(1, 0, 0) if left else Vector3(-1, 0, 0)
