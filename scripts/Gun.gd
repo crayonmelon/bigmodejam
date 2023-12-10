76474a54d3944e959e470506222c4d0d4878a72d
Ciarán Malone <ciaranmalone27@gmail.com>
2023-12-10 14:35:06 +0000

    better balance

--- scripts/Gun.gd
+++ scripts/Gun.gd
@@ -2,10 +2,14 @@ extends Node3D
 
 var enemy_bullet = preload("res://scenes/enemy_bullet.tscn")
 
+func _ready():
+	await get_tree().create_timer(4.0).timeout 
+	queue_free()
+
 func _on_timer_timeout():
 	var enemy = enemy_bullet.instantiate()
 	
 	enemy.position = global_position
 	enemy.rotation = global_rotation	
 	get_tree().root.add_child(enemy)
-
+	
